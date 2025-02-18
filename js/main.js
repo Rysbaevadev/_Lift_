1206efafc6da48181c58555ccd0cd77e1567a065
Latifa <latifarysbaeva2283@gmail.com>
2025-02-19 01:03:08 +0600

    .

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 let btn = document.querySelectorAll("#btn");
 let lift = document.querySelector("#lift");
-let floor = document.querySelectorAll(".floor");
-
 btn.forEach((el) => {
 	el.addEventListener("click", () => {
+		let floor2 = parseInt(el.textContent);
+		Lift(floor2);
+	});
+});
+
+function Lift(floor2) {
+	let position = document.getElementById(`floor${floor2}`);
+	let top = position.offsetTop;
+	lift.style.transition = "top 0.5s ease-in-out";
+	lift.style.top = `${top}px`;
+}
+btn.forEach((el) => {
+	el.addEventListener("mousemove", () => {
 		el.style.border = "2px solid red";
 		el.style.color = "red";
+	});
+});
+btn.forEach((el) => {
+	el.addEventListener("mouseout", () => {
+		el.style.border = "none";
+		el.style.color = "black";
 	});
 });
